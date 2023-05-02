ae3d11fd1af673bceafdd3e20cdb197ce4309843
tanvirsiraj <tanvirbinsiraj.edu.bd@gmail.com>
2023-05-03 01:23:17 +0600

    data sending from server

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+const allChef = require('./data/allChef.json');
 
 app.get('/', (req, res) => {
     res.send('Kitchen Explorer is running');
 })
 
+app.get('/allChef', (req, res) => {
+    res.send(allChef);
+})
 
 app.listen(port, () => {
     console.log(`Kitchen Explorer is running on Port: ${port}`);
